115a4222aced19595092f73dab0751d313ef6a15
Yuriy Ivanov <ymivanov@icloud.com>
2016-10-03 04:06:39 -0400

    Checkpoint 5 Complete

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -14,9 +14,30 @@
         });
     }
     
+    function BlocChatCookies($cookies, $uibModal) {
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        if (!currentUser || currentUser === '') {
+            var userModal = $uibModal.open({
+            // Modal configuration object properties
+                templateUrl: '/templates/username.html',
+                controller: function ($scope, $uibModalInstance) {
+                    $scope.newUserName = '';
+                    $scope.createUser = function() {
+                        $uibModalInstance.close($scope.newUserName);
+                    };
+                },
+                size: 'sm'
+            });
+            userModal.result.then(function(data) {
+                $cookies.put(currentUser, data);
+            });
+        }   
+    }
+    
     angular
-        .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase'])
-        .config(config);
+        .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase', 'ngCookies'])
+        .config(config)
+        .run(['$cookies', '$uibModal', BlocChatCookies])
     
     
 })();
